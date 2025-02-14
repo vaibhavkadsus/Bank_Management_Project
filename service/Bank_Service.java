bdbdfae311db6aa028db26f9507ffe572ef0106f
vaibhav <vaibhavkadus3131@gmail.com>
2025-02-14 10:22:38 +0530

    register form created

--- service/Bank_Service.java
+++ service/Bank_Service.java
@@ -1,11 +1,25 @@
 package com.service;
 
+import java.util.Scanner;
+
 import com.model.User;
 
 public class Bank_Service {
+	Scanner sc=new Scanner(System.in);
 	User u=new User();
 	
 	public void registerUser() {
 		System.out.println("Enter User Id...");
+		u.setUid(sc.nextInt());
+		System.out.println("Enter User Name...");
+		u.setUname(sc.next() + sc.nextLine());
+		System.out.println("Enter User Address...");
+		u.setAddress(sc.next() + sc.nextLine());
+		System.out.println("Enter Account number...");
+		u.setAcc_no(sc.nextLong());
+		System.out.println("Enter User Balance...");
+		u.setAcc_balance(sc.nextDouble());
+		return;
+		
 	}
 }
